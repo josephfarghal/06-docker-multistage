75f2ef8924fa9900d1125890ece88fe3c80cb5f3
josephfarghal <farghal.yousef@gmail.com>
2026-08-25 17:04:06 +0000

    Commented out thread sleeper

--- starter/src/main/java/com/neueda/leap/Main.java
+++ starter/src/main/java/com/neueda/leap/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world from the team's Sprint 1 project skeleton");
         System.out.println("Container is up. Sleeping so you can inspect it and check docker-compose networking.");
-        Thread.sleep(600_000);
+        //Thread.sleep(600_000);
     }
 }
